50fec921798970413561976397ac90f1d703d918
mcsu-mbp <sh.404@outlook.com>
2020-11-10 14:43:47 +0800

    <perf>:改善try逻辑

--- MultiPageCatch.py
+++ MultiPageCatch.py
@@ -10,7 +10,6 @@ class MultiPageCatch:  # 只负责抓取项目名称与项目地址 k-v name-add
         self.r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
     def url_download(self, url):  # 下载url的源码
-        try:
             if url is None:
                 return None
             browser = webdriver.Safari()
@@ -23,26 +22,32 @@ class MultiPageCatch:  # 只负责抓取项目名称与项目地址 k-v name-add
                 # 确定页码数， 每页项目数
                 for p in range(67):
                     for i in range(20):
-                        # 按需确定xpath
-                        print('第{}页第{}项'
-                              .format(browser
-                                      .find_element_by_xpath('//*[@id="MoreInfoList1_Pager"]/table/tbody/tr/td[1]/font[3]/b').text, i+1))
-                        html_text = browser.find_element_by_xpath(
-                            '//*[@id="MoreInfoList1_DataGrid1"]/tbody/tr[' + str(i + 1) +
-                            ']/td[2]/a').text
-                        html_href = browser.find_element_by_xpath(
-                            '//*[@id="MoreInfoList1_DataGrid1"]/tbody/tr[' + str(i + 1) +
-                            ']/td[2]/a').get_attribute('href')
-                        # k-v text-href
-                        # to-url
-                        # 标题判断
-                        if '公告' not  in html_text:
-                            # 使用redis存储
-                            self.r.sadd('todo-url', html_href)
-                            self.r.set(html_text, html_href)
-                            print('ok')
-                        else:
-                            print(html_text)
+                        try:
+                            # 按需确定xpath
+                            print('第{}页第{}项'
+                                  .format(browser
+                                          .find_element_by_xpath('//*[@id="MoreInfoList1_Pager"]/table/tbody/tr/td[1]/font[3]/b').text, i+1))
+                            html_text = browser.find_element_by_xpath(
+                                '//*[@id="MoreInfoList1_DataGrid1"]/tbody/tr[' + str(i + 1) +
+                                ']/td[2]/a').text
+                            html_href = browser.find_element_by_xpath(
+                                '//*[@id="MoreInfoList1_DataGrid1"]/tbody/tr[' + str(i + 1) +
+                                ']/td[2]/a').get_attribute('href')
+                            # 标题判断
+                            if '公告' not  in html_text:
+                                # 使用redis存储
+                                self.r.sadd('todo-url', html_href)
+                                self.r.set(html_text, html_href)
+                                print('ok')
+                            else:
+                                print(html_text)
+                        except selenium.common.exceptions.InvalidArgumentException:
+                            print("URL异常")
+                            return None
+                        except selenium.common.exceptions.NoSuchElementException:
+                            print('内容异常')
+                        except selenium.common.exceptions.StaleElementReferenceException:
+                            print('OTE')
                     # 翻页
                     page_xpath = ''
                     if 67 - p <= 67:
@@ -53,10 +58,6 @@ class MultiPageCatch:  # 只负责抓取项目名称与项目地址 k-v name-add
                         #//*[@id="MoreInfoList1_Pager"]/table/tbody/tr/td[2]/a[11]/img
                     # 缓冲时间
                     sleep(0.5)
-            return None
-        except selenium.common.exceptions.InvalidArgumentException:
-            print("URL异常")
-            return None
 
 
 if __name__ == "__main__":
